ca62cf12df9c42c984ef68d83b443b228293d8b3
Михаил <rodgdestwo@yandex.ru>
2022-10-03 23:52:31 +0300

    Готовая лаба 3

--- module/DashboardLayout/MainView.js
+++ module/DashboardLayout/MainView.js
@@ -1,14 +1,11 @@
 import * as ReactDOM from "react-dom/client";
 import * as React from 'react';
 import { StyledEngineProvider } from '@mui/material/styles';
-import Header from './components/Header/AppBar.js';
-import Main from './components/Main/ImageList.js';
-import Footer from './components/Footer/BottomNav.js';
-
-//ReactDOM.render(<App />, document.querySelector('#app'));
+import Header from '../components/Header/AppBar.js';
+import Main from '../components/Main/ImageList.js';
+import Footer from '../components/Footer/BottomNav.js';
 
 const app = document.querySelector("#app");
-//ReactDOM.render(<Header />, app);
 ReactDOM.createRoot(document.querySelector("#app")).render(
 <React.StrictMode>
     <StyledEngineProvider injectFirst>
--- module/components/Footer/BottomNav.js
+++ module/components/Footer/BottomNav.js
@@ -10,7 +10,7 @@ export default function SimpleBottomNavigation() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box size = "large">
       <BottomNavigation
         showLabels
         value={value}
@@ -18,7 +18,7 @@ export default function SimpleBottomNavigation() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />}/>
         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
         <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
       </BottomNavigation>
